cecd023fa181cf9f98bd163c2bee047f21ba1950
varadasainikhil <varadasainikhil@gmail.com>
2024-05-12 20:41:36 -0400

    Initial Commit

---
+++ Day-18/Day_18App.swift
@@ -0,0 +1,17 @@
+//
+//  Day_18App.swift
+//  Day-18
+//
+//  Created by Sai Nikhil Varada on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Day_18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
